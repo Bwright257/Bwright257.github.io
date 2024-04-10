0f109538325f09c042715f36a177a97879dc3297
Brandon <Phyra27@gmail.com>
2024-04-09 23:40:25 -0400

    test

--- assets/contact.php
+++ assets/contact.php
@@ -1,8 +1,14 @@
 <?php
     $to = "phyra27@gmail.com";
-    $subject = ;
+    $subject = "hello";
 
     $headers = array(
         "MIME-Version" => "1.0",
-        
-    )
+        "Content-Type" => "text/html; charset=UTF-8",
+        "From" => "test@mail.com",
+        "Reply-To" => "test@mail.com"
+    );
+
+    $message = file_get_contents("contact.html");
+
+    $send = mail($to, $subject, $message, $headers);
